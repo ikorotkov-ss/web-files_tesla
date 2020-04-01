a94d1fbdc137472f0caa1717b0ade11bf1fc717c
Igor <korotkovigorby@gmail.com>
2020-04-02 00:25:27 +0300

    Сделано всё, кроме адаптива

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 // функция отвечает за работу слайдера
-$(function(){
-    $('.slider').slick({
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        dots: true
-    });
+$(function() {
+  $(".slider").slick({
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true
+  });
+
+  //функция добавляет класс для menu по клику
+  $(".header-btn").on("click", function() {
+    $(".menu").addClass("active");
+  });
+
+  //функция добавляет класс для close-btn по клику
+  $(".close-btn").on("click", function() {
+    $(".menu").removeClass("active");
+  });
 });
